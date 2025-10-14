ada29c3e11de1c1d66a20a56cfba40f0857defbb
Fabrizio Guespe <fguespe@gmail.com>
2025-10-14 15:56:31 -0300

    Remove Header component

--- frontend/src/pages/Page.tsx
+++ frontend/src/pages/Page.tsx
@@ -2,7 +2,6 @@
 
 import { useEffect, useState } from "react";
 import { FullPageLoader } from "@/components/FullPageLoader";
-import { Header } from "@/components/Header";
 import { SafeAreaContainer } from "@/components/SafeAreaContainer";
 import MemberRenderer from "@/components/MemberRenderer";
 
@@ -28,8 +27,7 @@ export default function Page() {
   return (
     <SafeAreaContainer>
       <div className="flex flex-col w-full max-w-md mx-auto h-screen bg-black">
-        <Header isConnected={false} />
-
+    
         <div className="flex flex-col gap-4 px-4 py-4 h-full overflow-auto">
           <div className="mb-4">
             <h3 className="text-xl font-bold text-white mb-2">
